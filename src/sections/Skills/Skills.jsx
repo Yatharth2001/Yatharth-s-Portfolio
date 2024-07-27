88e0e8ff0af79a20085e07cb41f5f514accb01e3
Yatharth Shah <yatharhshah00@gmail.com>
2024-07-26 23:26:12 -0400

    make some changes

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -7,11 +7,11 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="TypeScript" />
         <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="HTML" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
       </div>
       <hr />
       <div className={styles.skillList}>
